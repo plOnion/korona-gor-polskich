9ed2a98aaeed629abef5f3ebd370efb584487073
plOnion <damian.lukasiewicz@gmail.com>
2019-07-02 14:03:37 +0200

    App components added - main list

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Main from "./Main"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Korona gór polskich</h1>
+      </header>
+      <main>
+        <Main />
+      </main>
+    </div>
+  );
+}
+
+export default App;
